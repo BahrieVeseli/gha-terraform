9c9d6da635646414cc3942bfdcf30afa89d56759
BahrieVeseli <bahrieveseli1@gmail.com>
2026-06-11 14:43:02 +0200

    Specify subnet for EC2 instance

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "webapp" {
   ami           = "ami-0f1834be8d049e69f"
   instance_type = "t3.micro"
-  subnet_id     = "subnet-00eb6155728d3375b"
+  subnet_id     = "subnet-0b7d96e849930c796"
 
   tags = {
     Name  = "bahrie-webapp"
